297477dd45d195e8915d33261c60d142a6f06d27
Justin Lyman <justinlyman@gmail.com>
2015-10-02 04:03:39 -0600

    Add like counter field to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151002035813) do
+ActiveRecord::Schema.define(version: 20151002091709) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
@@ -38,8 +49,9 @@ ActiveRecord::Schema.define(version: 20151002035813) do
   create_table "posts", force: :cascade do |t|
     t.string   "body"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "likers_count", default: 0
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
